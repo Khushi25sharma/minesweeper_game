7742123adeb1a5ca18f65de752b3fdad4492e938
Khushi25sharma <khushisharma110021@gmail.com>
2025-07-02 17:40:41 +0530

    Initial commit: Add Minesweeper game files

---
+++ script.js
@@ -0,0 +1,12 @@
+let chosen_grid_size = document.getElementById("GridSizeInput");
+let input_value = chosen_grid_size.value;
+
+let startGame=document.getElementById("StartGame");
+
+startGame.addEventListener ("click" , () => {
+    console.log("button");
+})
+
+
+
+
