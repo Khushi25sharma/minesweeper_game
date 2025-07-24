664b30698b115734fc86f94e66075bea6b5a88d1
Khushi25sharma <khushisharma110021@gmail.com>
2025-07-24 22:08:01 +0530

    chore: commit all recent changes across all files

--- script.js
+++ script.js
@@ -1,62 +1,84 @@
 let startGame=document.getElementById("StartGame");
-function creatingGrid(){
+
+
+function Taking_Grid_Input(){
     let chosen_grid_size = document.getElementById("GridSizeInput");
     let input_value = chosen_grid_size.value;
-    // console.log(input_value);
+    return input_value;
+}
+
+function Taking_Mines_Input(){
+    let mines_size = document.getElementById("NoOfMinesInput");
+    let input_mines_size = mines_size.value;
+    return input_mines_size;
+}
+
+
+
+
+function creatingGrid(){
+    // let chosen_grid_size = document.getElementById("GridSizeInput");
+    // let input_value = chosen_grid_size.value;
+    let user_input = Taking_Grid_Input();
+    console.log(user_input);
     let Game=document.getElementById("maingame");
 
-        for(let i=0; i<input_value; i++)
+    function blockClick(block){
+        console.log(block.id);
+        // if(block.id == position_i);
+        // {
+        //     console.log(block.id);
+        // }
+
+    }
+
+        for(let i=0; i<user_input; i++)
         {
             NewRow=document.createElement("div");
             NewRow.setAttribute('id',  'row-' + (i));
             NewRow.classList.add('grid', 'grid-cols-auto','grid-flow-col-dense', 'gap-4',   'bg-pink-300' , 'hover:bg-amber-200', 'rounded-lg', 'm-8');
    
-            for(let j=0; j<input_value; j++)
+            for(let j=0; j<user_input; j++)
             {
                 NewCol=document.createElement("div");
-                NewCol.setAttribute('id', 'grid-'+(i)+(j));
+                NewCol.setAttribute('id', ''+(i)+(j));
                 NewCol.classList.add('grid', 'grid-rows-10', 'grid-flow-row-dense', 'gap-4',   'bg-indigo-950' , 'hover:bg-pink-900', 'rounded-lg', 'm-4');
+                NewCol.onclick= blockClick(NewCol);
                 NewRow.appendChild(NewCol);
+                
             }
 
             Game.appendChild(NewRow);
         }
 
-    // function mines_image{
-    // let imageset= document.createElement("img");
-    // imageset.src= "mines.jpg";
-    // imageset.alt = 'Description of the image';
-    // imageset.setAttribute('id', 'newDivId');
-    // imageset.width = 200;
-    // imageset.height = 150;
-    // }
 
-
-    function getRandomGridPosition(input_value , input_value ) {
-        const randomRowNo = Math.floor(Math.random() * input_value);
-        const randomColNo = Math.floor(Math.random() * input_value);
+    function getRandomGridPosition(user_input , user_input ) {
+        const randomRowNo = Math.floor(Math.random() * user_input);
+        const randomColNo = Math.floor(Math.random() * user_input);
         return { row: randomRowNo, col: randomColNo };
     }
     
     
     function Postions_for_Mines(){
     
-        let mines_size = document.getElementById("NoOfMinesInput");
-        let input_mines_size = mines_size.value;
+        // let mines_size = document.getElementById("NoOfMinesInput");
+        // let input_mines_size = mines_size.value;
+
+        let user_mines_input = Taking_Mines_Input();
+        console.log(user_mines_input);
         
-        for(let i=1; i<=input_mines_size; i++)
+        for(let i=1; i<=user_mines_input; i++)
         {
-            const position_i = getRandomGridPosition(input_value, input_value);
+            const position_i = getRandomGridPosition(user_input, user_input);
             console.log(position_i);
-            // newarray.push(position_i);
-            // console.log(newarray);
         }
     
     }
     Postions_for_Mines();
 }
 
-
 startGame.addEventListener ("click" , () => {
-    creatingGrid();
- });
+//     let user_input = Taking_Grid_Input();
+// console.log(user_input);
+creatingGrid()
+});
